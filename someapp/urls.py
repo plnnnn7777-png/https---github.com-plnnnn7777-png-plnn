68cddf9f2061ec99f89ac6647402995677a1f70a
plnnnn7777-png <plnnnn7777@gmail.com>
2025-12-09 20:33:32 +0500

    Delete someapp directory

--- someapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("services/<slug:slug>/", views.service_detail, name="service_detail"),
-]
